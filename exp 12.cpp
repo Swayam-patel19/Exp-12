8f6740795bbd20c49a953348cb45d1436da7f4c9
Swayam Patel <123808643+Swayam-patel19@users.noreply.github.com>
2025-04-24 22:44:35 +0530

    Add files via upload

---
+++ exp 12.cpp
@@ -0,0 +1,80 @@
+#include<iostream> 
+#include<string>
+using namespace std; 
+class Data {
+    string name;
+    int roll_no;
+    char dept[50];
+    char division;
+
+    public:
+    Data() {
+        cout<<"Name: ";
+        cin>>name;
+        cout<<"Roll Number: ";
+        cin>>roll_no;
+        cout<<"Department: ";
+        cin>>dept;
+        cout<<"Division: ";
+        cin>>division;
+    }
+    void display() {
+        cout<<"\n"<<"DETAILS:"<<"\n"<<name<<"  "<<roll_no<<"  "<<dept<<"  "<<division<<"\n";
+    }
+};
+int main() 
+{
+    Data d1;
+    d1.display();
+} 
+
+#12B
+
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater.";
+        }
+        else 
+        {
+            cout<<d<<" is greater.";
+        }
+    }
+};
+int main()
+{
+    Num n1(4,3);
+} 
+
+#12C
+
+#include<iostream>
+using namespace std;
+int count=0;
+class destruct{
+    public:
+    destruct()
+    {
+        count++;
+        cout<<"Number of objects created: "<<count<<"\n";
+    }
+    ~destruct()
+    {
+        count--;
+        cout<<"Number of objects destroyed: "<<count<<"\n";
+    }
+};
+int main()
+{
+    destruct aa,bb,cc;
+    {
+        destruct dd;
+    }
+    return 0;
+}
